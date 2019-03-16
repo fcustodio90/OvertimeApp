11b44ad9c2e150063e6c4cba9414238672cc5689
filipe custodio <filipe.l.custodio@gmail.com>
2019-03-16 19:32:48 +0000

    Implemented enum for status into posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :date, :rationale
+  # submitted / approved / rejected /
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
 end
+
+
