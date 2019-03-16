404fb497df15740c8d37e024f6291904c11305e7
filipe custodio <filipe.l.custodio@gmail.com>
2019-03-16 22:28:16 +0000

    Implemented edit functionality for status

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -14,6 +14,7 @@ class PostDashboard < Administrate::BaseDashboard
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -24,6 +25,7 @@ class PostDashboard < Administrate::BaseDashboard
   COLLECTION_ATTRIBUTES = [
     :user,
     :id,
+    :status,
     :date,
     :rationale,
   ].freeze
@@ -34,6 +36,7 @@ class PostDashboard < Administrate::BaseDashboard
     :user,
     :id,
     :date,
+    :stauts,
     :rationale,
     :created_at,
     :updated_at,
