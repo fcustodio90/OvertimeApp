05c87a91958ae203e169a0ee1517aca59c466c34
filipe custodio <filipe.l.custodio@gmail.com>
2019-04-08 18:06:57 +0100

    Implemented the ability to validate overtime_request db field with validations and changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 admin = AdminUser.create(email: 'admin@test.com', password: 123456, password_confirmation: 123456, first_name: 'Admin', last_name: 'User')
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
