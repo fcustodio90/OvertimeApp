88f2c1fd021b29ca76a61c40fcd3224193fecbc2
filipe custodio <filipe.l.custodio@gmail.com>
2019-04-08 18:07:47 +0100

    Implemented overtime_request for test suite and refactored a couple tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: 'rationale', user_id: user.id)
+    Post.create(date: Date.today, rationale: 'rationale', user_id: user.id, overtime_request: 3.5)
   end
 
   before do
@@ -36,7 +36,7 @@ describe 'navigate' do
                                email: 'asdfasdf@asdf.com',
                                password: 123456,
                                password_confirmation: 123456)
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
       visit posts_path
       expect(page).to_not have_content(/This post shouldn't be seen/)
     end
@@ -55,7 +55,7 @@ describe 'navigate' do
       logout(:user)
       delete_user =  FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
-      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id, overtime_request: 3.5)
       visit posts_path
       click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
@@ -73,13 +73,14 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-      click_on 'Save'
-      expect(page).to have_content('Some rationale')
+      fill_in 'post[overtime_request]', with: 4.5
+      expect { click_on 'Save' }.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated with it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'User Association'
+      fill_in 'post[overtime_request]', with: 4.5
       click_on 'Save'
       expect(User.last.posts.last.rationale).to eq('User Association')
     end
