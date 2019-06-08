a35d3d648549ebbb2a93af8c24e1df63bb4de615
filipe custodio <filipe.l.custodio@gmail.com>
2019-06-08 22:01:23 +0100

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,10 @@ admin = AdminUser.create(email: 'admin@test.com',
                          phone: '5555555555')
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "Now that there is the Tec-9, a crappy spray gun from South Miami.
+                                  This gun is advertised as the most popular gun in American crime. Do you believe that shit?
+                                  It actually says that in the little book that comes with it: the most popular gun in American crime.
+                                  Like they're actually proud of that shit.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
